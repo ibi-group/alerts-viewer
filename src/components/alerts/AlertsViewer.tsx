59846cceca50a6b7ac094f4115d7fd000e794b28
Amy Corson <115499534+amy-corson-ibigroup@users.noreply.github.com>
2026-08-17 18:21:02 -0500

    fix checkboxes initialize

--- src/components/alerts/AlertsViewer.tsx
+++ src/components/alerts/AlertsViewer.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './AlertsViewer.css';
 import type { Alert, AlertsViewerProps } from './types';
-import FilterOptions, { type PeriodEffectFilter } from './FilterOptions';
+import FilterOptions from './FilterOptions';
 import AlertList from './AlertList';
 import AlertBody from './AlertBody';
 
 interface AlertsViewerState {
   searchValue: string;
-  periodEffectFilter: PeriodEffectFilter;
+  showExpiredAlerts: boolean,
+  showNonExpiredAlerts: boolean,
   selectedAlert: Alert | null;
   alerts: Alert[];
   error: string | null;
@@ -18,7 +19,8 @@ export default class AlertsViewer extends React.Component<AlertsViewerProps, Ale
     super(props);
     this.state = {
       searchValue: '',
-      periodEffectFilter: null,
+      showExpiredAlerts: false,
+      showNonExpiredAlerts: true,
       selectedAlert: null,
       alerts: this.props.alerts ?? [],
       error: null,
@@ -60,15 +62,15 @@ export default class AlertsViewer extends React.Component<AlertsViewerProps, Ale
     ) ?? false;
   };
 
-  private matchesPeriodEffectFilter = (alert: Alert, periodEffectFilter: PeriodEffectFilter): boolean => {
-    if (!periodEffectFilter) {
-      return true;
+  private matchesPeriodEffectFilter = (alert: Alert, showExpiredAlerts: boolean, showNonExpiredAlerts: boolean): boolean => {
+    if (!showExpiredAlerts && !showNonExpiredAlerts) {
+      return false;
     }
 
     const now = Math.floor(Date.now() / 1000);
     const effectPeriods = alert.effect_periods ?? [];
 
-    if (periodEffectFilter === 'non-expired') {
+    if (showNonExpiredAlerts) {
       return effectPeriods.some((period) => {
         const start = Number(period.effect_start);
         const end = Number(period.effect_end);
@@ -85,7 +87,8 @@ export default class AlertsViewer extends React.Component<AlertsViewerProps, Ale
       });
     }
 
-    return effectPeriods.length > 0 && effectPeriods.every((period) => {
+    if (showExpiredAlerts) {
+      return effectPeriods.length > 0 && effectPeriods.every((period) => {
       const start = Number(period.effect_start);
       const end = Number(period.effect_end);
 
@@ -95,14 +98,16 @@ export default class AlertsViewer extends React.Component<AlertsViewerProps, Ale
 
       return end < now && start < now;
     });
+    }
+    return true;
   };
 
   private getFilteredAlerts = (): Alert[] => {
-    const { alerts, searchValue, periodEffectFilter } = this.state;
+    const { alerts, searchValue, showExpiredAlerts, showNonExpiredAlerts } = this.state;
 
     return alerts.filter((alert) => {
       const matchesSearch = this.matchesSearchFilter(alert, searchValue);
-      const matchesPeriodEffect = this.matchesPeriodEffectFilter(alert, periodEffectFilter);
+      const matchesPeriodEffect = this.matchesPeriodEffectFilter(alert, showExpiredAlerts, showNonExpiredAlerts);
       return matchesSearch && matchesPeriodEffect;
     });
   };
@@ -112,7 +117,7 @@ export default class AlertsViewer extends React.Component<AlertsViewerProps, Ale
   };
 
   render() {
-    const { searchValue, periodEffectFilter, selectedAlert } = this.state;
+    const { searchValue, showExpiredAlerts, showNonExpiredAlerts, selectedAlert } = this.state;
     const filteredAlerts = this.getFilteredAlerts();
     return (
       <div className="alerts-viewer">
@@ -122,9 +127,11 @@ export default class AlertsViewer extends React.Component<AlertsViewerProps, Ale
         <div className="alerts-viewer__content">
           <FilterOptions
             searchValue={searchValue}
-            periodEffectFilter={periodEffectFilter}
+            showExpiredAlerts={showExpiredAlerts}
+            showNonExpiredAlerts={showNonExpiredAlerts}
             onSearchChange={(value) => this.setState({ searchValue: value })}
-            onPeriodEffectFilterChange={(value) => this.setState({ periodEffectFilter: value })}
+            onExpiredAlertsChange={(value: any) => console.log(value)}
+            onNonExpiredAlertsChange={(value: any) => console.log(value)}
           />
           <div className="alerts-viewer__container">
             <AlertList
